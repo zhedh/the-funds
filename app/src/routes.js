a1d131cc502f2eab7dc32f5087202c8aa53e7736
zhengdahua <zhengdahua@imdada.cn>
2019-08-01 00:31:58 +0800

    create project catalogue

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,7 +1,33 @@
-import Home from './pages/home/Index'
-import InviterFriend from './pages/home/InviterFriend'
-import Login from './pages/login/Login'
-import Register from './pages/login/Register'
+import React, {Component} from 'react';
+import Loadable from 'react-loadable';
+
+class Loading extends Component {
+    render() {
+        return (
+            <div>
+                loading...
+            </div>
+        );
+    }
+}
+
+
+const Home = Loadable({
+    loader: () => import('./pages/home/Index'),
+    loading: Loading
+});
+const InviterFriend = Loadable({
+    loader: () => import('./pages/home/InviterFriend'),
+    loading: Loading
+});
+const Login = Loadable({
+    loader: () => import('./pages/login/Login'),
+    loading: Loading
+});
+const Register = Loadable({
+    loader: () => import('./pages/login/Register'),
+    loading: Loading
+});
 
 export default [
     {path: '/', name: 'Home', component: Home},
