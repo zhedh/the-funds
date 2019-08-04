e3d422a96a383df243a1015517726a5d1076c20e
zhengdahua <zhengdahua@imdada.cn>
2019-08-05 00:02:29 +0800

    update wang to dev

---
+++ app/src/pages/wallet/ZbxWithdraw.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Header from "../../components/common/Header";
+
+import './ZbxWithdraw.scss';
+
+class ZbxWithdraw extends Component {
+  render() {
+    return (
+      <div id="zbs-withdraw">
+        <Header title="ZBX提币" isFixed isShadow/>
+
+      </div>
+    );
+  }
+}
+
+export default ZbxWithdraw;
