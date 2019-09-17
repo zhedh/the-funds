c2fbd30d07ebdb3dd490b3e76a7011a7322c8784
vera <wanghemeng@viinet.com>
2019-09-17 12:35:55 +0800

    ipdate

--- app/src/assets/static/xc.js
+++ app/src/assets/static/xc.js
@@ -16,7 +16,7 @@ const XC = {
     GENERALIZE_BG: require('../images/xc/generalize-banner.jpg'),
     GENERALIZE_LEVEL: ['无', '普通', '初级', '中级', '高级', '超级'],
     GENERALIZE_USER_ONE_ICON: require('../images/xc/generalize-user-one.png'),
-    GENERALIZE_USER_TWO_ICON: require('../images/xc/generalize-user-two.png'),
+    GENERALIZE_USER_TWO_ICON: require('../images/xc/generalize-user-two.png')
   },
   BARGAIN: {
     BANNER_LABEL: '特价XC奖励额度：'
@@ -31,12 +31,13 @@ const XC = {
     IMG_WAIT: require('../images/xc/auth-wait.png'),
     IMG_SUCCESS: require('../images/xc/auth-success.png'),
     IMG_FAIL: require('../images/xc/auth-fail.png'),
-    UPLOAD_WRITE: 'X PLAN',
+    UPLOAD_WRITE: 'X PLAN'
   },
   DEPOSIT: {
+    COIN_NAME: '参与节点',
     TITLE: 'X PLAN',
     TABS: ['X PLAN', '特价额度'],
-    SIDEBAR_TITLE: '选择参与计划',
+    SIDEBAR_TITLE: '选择参与计划'
   },
   USER: {
     CUSTOMERS: []
